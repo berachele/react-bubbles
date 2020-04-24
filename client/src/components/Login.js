b154b41445403bbc08c33a5f26e49a949182e44a
Rachele Edwards <dot.com1472@gmail.com>
2020-04-24 09:53:54 -0600

    added answers to readme questions, added axiosWithAuth and PrivateRout, stage one is complete and can successfully gain a token and login to the BubblePage

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,67 @@
 import React from "react";
+import {axiosWithAuth} from "../utils/axiosWithAuth"
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
-  return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
-};
+class Login extends React.Component{
+  // make a post request to retrieve a token from the api✅
+  // when you have handled the token, navigate to the BubblePage route✅
+  state = {
+    logInfo: {
+      username: "",
+      password: ""
+    }
+  }
+
+  handleChange = e => {
+    this.setState({
+      logInfo: {
+        ...this.state.logInfo,
+        [e.target.name]: e.target.value
+      }
+    })
+  }
+
+  login = e => {
+    e.preventDefault()
+    axiosWithAuth()
+      .post("/api/login", this.state.logInfo)
+      .then(res => {
+        console.log({res})
+        //res/data/payload
+        localStorage.setItem('token', JSON.stringify(res.data.payload))
+        this.props.history.push("/bubbles")
+      })
+      .catch(err => {
+        console.log({err})
+      })
+  }
+
+render(){
+    return (
+      <>
+        <h1>Welcome to the Bubble App!</h1>
+        <p>Build a login page here</p>
+        <div className="login-form">
+          <form onSubmit={this.login}>
+            <input 
+            type="text"
+            name="username"
+            placeholder="Username"
+            value={this.state.logInfo.username}
+            onChange={this.handleChange}
+            />
+            <input 
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={this.state.logInfo.password}
+            onChange={this.handleChange}
+            />
+            <button>Log in</button>
+          </form>
+        </div>
+      </>
+    );
+  };
+}
 
 export default Login;
